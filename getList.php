22ffb0121e8017ee1687619b50f2d4fba62ee934
slimeVidas26 <dahan.isaac@gmail.com>
2018-10-14 21:51:02 +0300

    getList

---
+++ getList.php
@@ -0,0 +1,18 @@
+<?php
+$list = [
+    [
+        'id'=> 1,
+        'name'=> 'Steven Hawking'
+    ],
+    [
+        'id'=> 2,
+        'name'=> 'Homer Simpson'
+    ],
+    [
+        'id'=> 3,
+        'name'=> 'Prince Ess'
+    ]
+
+    ];
+
+    echo json_encode($list);
