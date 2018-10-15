e5f2c79998250f754711e5b90ceb1a164177b799
slimeVidas26 <dahan.isaac@gmail.com>
2018-10-15 22:51:18 +0300

    show list of persons

---
+++ getList.php
@@ -0,0 +1,20 @@
+<?php
+
+$list = [
+   [
+       "id"=> 1,
+       "name"=> "Steven Hawkins"
+   ],
+   [
+    "id"=> 2,
+    "name"=> "Homer Simpson"
+],
+[
+    "id"=> 3,
+    "name"=> "Henry Miller"
+    ]
+];
+
+echo json_encode($list);
+
+
--- script.js
+++ script.js
@@ -1,5 +1,46 @@
 $(function(){
 
+    function loadList(){
+        // display onLoad the list of persons listed in the getList.php file
+        // create the getList.php file
+        // calling in the browser the getList.php file will display this result below:
+        // [
+        //     {"id":1,"name":"Steven Hawkins"},
+        //     {"id":2,"name":"Homer Simpson"},
+        //     {"id":3,"name":"Henry Miller"}
+        // ]
+        // ajax request call to display in the list div the list of persons
+
+        $.ajax({
+            url : 'getList.php',
+            method : 'GET',
+            dataType : 'json'
+        }).done(function(r){
+            $ul = $('#list ul');
+            console.log(r);
+            for(let i in r){
+            $(`<li data-id =${r[i].id} >${r[i].name}</li>`).on('click',function(){
+                showPerson($(this).data('id'))
+            }).appendTo($ul);
+            }
+            $('#person').hide();
+            $('#list').show();
+
+        })
+    }
+
+    function showPerson(pid){
+        console.log('id',pid);
+    }
+
+    
+
+    
+
+
+
+    loadList();
+
 
 
 
