0c056ee5a669106dbd1be90b81c165c5b6914141
slimeVidas26 <dahan.isaac@gmail.com>
2018-10-16 21:09:21 +0300

    implement edit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-$(function(){
+$(function () {
 
-    function loadList(){
+    function loadList() {
         // display onLoad the list of persons listed in the getList.php file
         // create the getList.php file
         // calling in the browser the getList.php file will display this result below:
@@ -12,50 +12,90 @@ $(function(){
         // ajax request call to display in the list div the list of persons
 
         $.ajax({
-            url : 'getList.php',
-            method : 'GET',
-            dataType : 'json'
-        }).done(function(r){
+            url: 'getList.php',
+            method: 'GET',
+            dataType: 'json'
+        }).done(function (r) {
             $ul = $('#list ul');
             console.log(r);
-            for(let i in r){
-            $(`<li data-id =${r[i].id} >${r[i].name}</li>`).on('click',function(){
-                showPerson($(this).data('id'));
-            }).appendTo($ul);
+            for (let i in r) {
+                $(`<li data-id =${r[i].id} >${r[i].name}</li>`).on('click', function () {
+                    showPerson($(this).data('id'));
+                }).appendTo($ul);
             }
             $('#list').show();
 
         })
     }
 
-    function showPerson(pid){
+    function showPerson(pid) {
         //console.log('id',pid);
         //this function will make a request to the getPerson.php file
         //create getPerson.php file
         //we want to display the details of the person that has ID = pid
         //this ID is provided by the data-id of the <li>
         $.ajax({
-            url:"getPerson.php",
+            url: "getPerson.php",
             method: "GET",
-            data:{id:pid},
+            data: {
+                id: pid
+            },
             dataType: "json"
 
-        }).done(function(r){
+        }).done(function (r) {
             console.log(r);
-            $("#person h3").text(r.data.name);
-            $("#person .email").text(r.data.email);
-            $("#person .phone").text(r.data.phone);
+            if (!r.success) {
+                $('#notify').show();
+            } else {
+                //display person details
+                $("#person h3").text(r.data.name);
+                $("#person .email").text(r.data.email);
+                $("#person .phone").text(r.data.phone);
+                $("#list").hide();
+                $("#person").show();
 
-            
-            $("#list").hide();
-            $("#person").show();
+                //show filled form
+                var es = document.forms.edit.elements;
+                //console.log(es);
+                es.name.value = r.data.name;
+                es.email.value = r.data.email;
+                es.phone.value = r.data.phone;
+
+
+            }
 
         });
     }
 
-    
+    function validateForm(){
+        
+    }
 
-    
+    $('#person .back').on('click', function () {
+        $('#list').show();
+        $('#person').hide();
+    });
+
+    $('#edit .back').on('click', function () {
+        $('#edit').hide();
+        $('#person').show();
+    });
+
+    $('.edit').on('click', function () {
+        $('#person').hide();
+        $('#edit').show();
+
+    });
+
+    $('.save').on('click', function () {
+        $('#edit').hide();
+        $('#list').show();
+
+    });
+
+
+
+
 
 
 
